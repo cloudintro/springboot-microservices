136aff0b3b76987e753a0b6aa11303b65f389fc8
Sushil Prasad <cse.sushil@gmail.com>
2020-09-20 22:27:58 +0530

    spring boot admin-client and micrometer

---
+++ employee-mgr/src/main/java/com/smallintro/springboot/config/MonitorConfig.java
@@ -0,0 +1,8 @@
+package com.smallintro.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitorConfig {
+
+}
---
+++ springboot-admin/src/main/java/com/smallintro/spring/SpringbootAdminApplication.java
@@ -0,0 +1,16 @@
+package com.smallintro.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringbootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminApplication.class, args);
+	}
+
+}
