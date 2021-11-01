a1cb67e180d736b39d09c789c4fcd4eec60d2933
Sushil Prasad <cse.sushil@gmail.com>
2021-11-02 01:03:43 +0530

    code refactoring done

---
+++ api-gateway/src/main/java/io/github/smallintro/gateway/filter/PostFilter.java
@@ -0,0 +1,38 @@
+package io.github.smallintro.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PostFilter extends ZuulFilter {
+    Logger logger = LoggerFactory.getLogger(PostFilter.class);
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info("POST: URI:{}, Method:{}",request.getRequestURI(), request.getMethod());
+        return null;
+    }
+}
--- employee-service/src/main/java/io/github/smallintro/springboot/employeeservice/utils/ProjectConstants.java
+++ employee-service/src/main/java/io/github/smallintro/springboot/employeeservice/utils/ProjectConstants.java
@@ -5,11 +5,4 @@ public interface ProjectConstants {
 	public static final String OPERATION_SUCCESS = "Operation is success";
 	public static final String OPERATION_FAILED = "Operation is failed";
 
-	public static final String EXCEL_FILE_SUFFIX = ".xlsx";
-	
-	public static final String DATA_TYPE_DEFECTS = "defects";
-
-	public static final String NO_RECORD_FOUND = "No record found";
-	
-	
 }
